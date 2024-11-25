3f1e3c4450de29e48a1b881c1f486d7b95b26d77
Junwon Choi <junwon.choi@stu.ocsb.ca>
2024-11-25 15:49:15 +0000

    e

--- assignment.py
+++ assignment.py
@@ -1,6 +1,16 @@
 def calculatePi(digits):
   result = 0
+
+  for x in range(digits):
+    term = 4 * ((-1) ** x) / (2 * x + 1)
+    result += term
+    print(f"Iteration {x + 1}: {result}")
   return result
 
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+def main():
+  try:
+    digits = int(input("Invalid input, please input a integer: "))
+    if digits < 1:
+      print("Invalid input, please input a positive integer: ")
+    return
+
