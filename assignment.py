f473fb77d98df89f06cdbb518dcfee531283f324
Junwon Choi <junwon.choi@stu.ocsb.ca>
2024-12-03 09:47:53 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -6,10 +6,10 @@ def calculatePi(digits):
       return result
     for x in range(int(digits)):
       x = x - 1
-      result = result + 4 * ((-1) ** x) / (2 * x + 1)
+      result = result + ((-1) ** x) / (2 * x + 1)
   except:
     result = "Invalid input"
   return result
 
 numberofdigits = input()
-print(calculatePi(numberofdigits))
+print(4 * calculatePi(numberofdigits))
